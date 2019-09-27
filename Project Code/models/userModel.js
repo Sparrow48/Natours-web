7f9928b0b93ebc57832fee781f0ea28a462091b3
Sparrow48 <abu.nasib@northsouth.edu>
2019-09-27 22:59:11 +0600

    user signup and login.

--- Project Code/models/userModel.js
+++ Project Code/models/userModel.js
@@ -42,8 +42,13 @@ userSchema.pre('save',async function(next){
     this.password = await bcrypt.hash(this.password,12);
     this.passwordConfirm = undefined;
     next();
-})
+});
+
+
+
+userSchema.methods.correctPassword = async function(candidatePass,usrePass){
+    return await bcrypt.compare(candidatePass,usrePass);
+};
 
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
