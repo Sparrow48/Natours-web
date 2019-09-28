6b125808f9201d07d21d86c0de032db033150ba0
Sparrow48 <abu.nasib@northsouth.edu>
2019-09-28 22:20:17 +0600

    Complete authorization.

--- Project Code/controllers/authController.js
+++ Project Code/controllers/authController.js
@@ -78,3 +78,12 @@ exports.protect = catchAsync( async(req,res,next)=>{
     next();
   
 });
+
+exports.restricTo = (...roles)=>{
+    return (req,res,next)=>{
+        if(!roles.includes(req.user.role)){
+            return next(new AppError('You do not have permission to perform this action',401));
+        };
+        next();
+    };
+};
